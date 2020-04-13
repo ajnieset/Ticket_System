90f41a413ae1f6e97848d18380b87cceae730d44
Austin Nieset <ajnieset@gmail.com>
2020-04-13 18:27:10 -0400

    first commit

---
+++ database/migrations/2020_04_11_202924_create_tickets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('number');
+            $table->text('description');
+            $table->string('assigned');
+            $table->date('date_created_date');
+            $table->date('date_completed');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
