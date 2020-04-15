7abd4ce275e99d7c3c908e4a5726b0930902cb82
Austin Nieset <ajnieset@gmail.com>
2020-04-14 23:35:28 -0400

    Updated base and index views

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -1,3 +1,10 @@
 @extends('base')
 
 @section('main')
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-6">
+            <a href="{{ url('ticket/create') }}" class="btn btn-info"> <i class="fas fa-plus-circle"></i>Create Ticket </a>
+        </div>
+    </div>
+</div>
