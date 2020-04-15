5e6fe5827b44649f2c7574a180ce0bdaa0f015a0
Austin Nieset <ajnieset@gmail.com>
2020-04-15 00:42:23 -0400

    Added view single record button

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('main')
 <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
-<div class="container mt-3">
+<div class="container mt-5">
     <div class="row">
-        <div class="col-6">
+        <div class="col">
             <a href="{{ url('ticket/create') }}" class="btn btn-info"> <i class="fas fa-plus-circle"></i> Create Ticket </a>
         </div>
     </div>
@@ -31,11 +31,13 @@
                 <td> {{ $ticket->created_date }} </td>
                 <td> {{ $ticket->completed_date }} </td>
                 <td> {{ $ticket->status }} </td>
-                <td> 
+                <td>
+                    <a href="{{ route('ticket.show', $ticket->id )}}" class="badge badge-info p-2 mb-2"> View </a>
+                    <a href="{{ route('ticket.edit', $ticket->id )}}" class="badge badge-warning p-2 mb-2"> Edit </a>
                     <form action="{{ route('ticket.destroy', $ticket->id)}}" method="post">
                     {{ csrf_field() }}
                     @method('DELETE')
-                        <button class="badge btn-danger" type="submit"> Delete </button>
+                        <a class="badge btn-danger p-2" type="submit"> Delete </a>
                    </form>
                 </td>
             </tr>
